5e1544af0a66a463a459ea7cad8c8ba15ddca9dc
Emily Watkins and Hannah Lee <emily@>
2018-02-20 10:12:55 -0800

    continue from lesson, add and pass integration specs, add id equal nil.

--- app.rb
+++ app.rb
@@ -2,22 +2,43 @@ require("sinatra")
 require("sinatra/reloader")
 also_reload("lib/**/*.rb")
 require("./lib/task")
+require("./lib/list")
 require("pg")
 
 # DB = PG.connect({:dbname => "to_do"}) #connect to NON-TEST db
 
 DB = PG.connect({:dbname => "to_do_test"}) # change back to above when done testing
 
-get("/lists/:id") do # route to view a specific list
-  @list = List.find(params.fetch("id").to_i())
-  erb(:list)
+get('/') do
+  erb(:index)
 end
 
-post("/tasks") do
-  description = params.fetch("description")
-  list_id = params.fetch("list_id").to_i()
-  @list = List.find(list_id)
-  @task = Task.new({:description => description, :list_id => list_id})
-  @task.save()
-  erb(:task_success)
+get('/lists/new') do
+  erb(:list_form)
 end
+
+post('/lists') do
+  name = params.fetch('name')
+  list = List.new({:name => name, :id => nil})
+  list.save()
+  erb(:success)
+end
+
+get('/lists') do
+  @lists = List.all()
+  erb(:lists)
+end
+
+# get("/lists/:id") do # route to view a specific list
+#   @list = List.find(params.fetch("id").to_i())
+#   erb(:list)
+# end
+#
+# post("/tasks") do
+#   description = params.fetch("description")
+#   list_id = params.fetch("list_id").to_i()
+#   @list = List.find(list_id)
+#   @task = Task.new({:description => description, :list_id => list_id})
+#   @task.save()
+#   erb(:task_success)
+# end
