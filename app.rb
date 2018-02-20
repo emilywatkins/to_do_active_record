bf3928928e064bb3527e8bdbbdaf9fbad3e7642e
Emily Watkins and Hannah Lee <hannah@>
2018-02-20 11:16:24 -0800

    add post tasks that add tasks to the list.

--- app.rb
+++ app.rb
@@ -34,11 +34,11 @@ get("/lists/:id") do # route to view a specific list
   erb(:list)
 end
 
-# post("/tasks") do
-#   description = params.fetch("description")
-#   list_id = params.fetch("list_id").to_i()
-#   @list = List.find(list_id)
-#   @task = Task.new({:description => description, :list_id => list_id})
-#   @task.save()
-#   erb(:task_success)
-# end
+post("/tasks") do
+  description = params.fetch("description")
+  list_id = params.fetch("list_id").to_i()
+  @list = List.find(list_id)
+  @task = Task.new({:description => description, :list_id => list_id})
+  @task.save()
+  erb(:list)
+end
